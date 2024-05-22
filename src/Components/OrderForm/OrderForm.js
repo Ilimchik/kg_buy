d2c32e6b511895490e7568b51a055e1c60705531
Ilimchik <ilimbork@gmail.com>
2024-05-22 15:32:24 +0600

    ch button type submit

--- src/Components/OrderForm/OrderForm.js
+++ src/Components/OrderForm/OrderForm.js
@@ -4,7 +4,7 @@ import { ordersCollection } from "../../firebase";
 import { AppContext } from "../../App";
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from "../Button/Button";
+// import Button from "../Button/Button";
 
 export default function OrderForm({ active, setActive }) {
   const { cart, setCart, user } = useContext(AppContext);
@@ -52,7 +52,7 @@ export default function OrderForm({ active, setActive }) {
               <input type="text" name="address" placeholder="Country, city, street" required />
             </label>
             <div>
-              <Button>Submit</Button>
+              <button type="submit">Submit</button>
             </div>
           </form>
         </div>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+// import { useContext, useState } from "react";
 import "./Search.css"
 // import { AppContext } from "../../App";
 
