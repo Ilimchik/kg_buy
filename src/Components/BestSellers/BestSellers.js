5e8bf88ed4ff5163b85c5d3aadde2514d87e741e
Ilimchik <ilimbork@gmail.com>
2024-05-24 14:35:10 +0600

    add best sellers

---
+++ src/Components/BestSellers/BestSellers.js
@@ -0,0 +1,44 @@
+import "./BestSellers.css"
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+import { AddToCart } from "../AddToCart/AddToCart";
+import AddProduct from "../AddProduct/AddProduct";
+import DeleteProduct from "../DeleteProduct/DeleteProduct";
+
+export default function AllProducts({sellers}) {
+
+    const { products } = useContext(AppContext);
+    const output = products.filter(product => product.name)
+        .map(product => (
+            <div key={product.id} className="product">
+                <Link to={"/products/" + product.slug}>
+                    <img src={product.picture} alt={product.name} />
+                </Link>
+                <hr />
+                <Link to={"/products/" + product.slug} className="text">
+                    {product.name}
+                </Link>
+
+                <br />
+
+                <div className="actions">
+
+                    <span>${product.price}</span>
+                    <AddToCart product={product} />
+                </div>
+                <DeleteProduct product={product} />
+            </div>
+        ));
+
+    return (
+        <div className="ProductList">
+            {output}
+
+            <AddProduct category={category} />
+        </div>
+    )
+
+
+
+}
