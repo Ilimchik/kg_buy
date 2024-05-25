ebe6f9cd4a649cd6419669f37efefb424767a3b7
Ilimchik <ilimbork@gmail.com>
2024-05-25 18:44:48 +0600

    ch swiper version

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,20 +1,20 @@
 import './Menu.css';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Menu() {
   return (
     <nav className='Menu'>
-      <ul className='flex'>
-        <li>
+      <ul className='MenuContent flex'>
+        <li className='active'>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className='active'>
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li className='active'>
           <Link to="/delivery">Delivery</Link>
         </li>
-        <li>
+        <li className='active'>
           <Link to="/contacts">Contacts</Link>
         </li>
       </ul>
