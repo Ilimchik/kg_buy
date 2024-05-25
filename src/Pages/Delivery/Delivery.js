76dacab1a586acf61df96fcd8fd6d59f8b507dfa
Ilimchik <ilimbork@gmail.com>
2024-05-25 17:15:21 +0600

    add description to delivary page

--- src/Pages/Delivery/Delivery.js
+++ src/Pages/Delivery/Delivery.js
@@ -6,15 +6,15 @@ function Delivery() {
     <section className="Delivery flex">
       <div className="DelCard">
         <img className='DelImg' src={image} alt="" />
-        <div className="DelTxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum tempora suscipit adipisci ad ab, molestiae, voluptate error atque officiis similique tenetur quisquam non et reiciendis.</div>
+        <div className="DelTxt">Receive your orders at lightning speed! Our delivery service ensures prompt dispatch and fast delivery right to your doorstep. Whether you need food, household goods or gifts, we will do our best to ensure your order arrives as quickly and safely as possible.</div>
       </div>
       <div className="DelCard">
         <img className='DelImg' src={image} alt="" />
-        <div className="DelTxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum tempora suscipit adipisci ad ab, molestiae, voluptate error atque officiis similique tenetur quisquam non et reiciendis.</div>
+        <div className="DelTxt">Join our green delivery initiative! We're committed to making the world a better place, starting with reducing the carbon footprint of our deliveries. We use electric vehicles, biodegradable packaging and optimized delivery routes to reduce our environmental impact. Order today and together we will take a step towards a greener future!</div>
       </div>
       <div className="DelCard">
         <img className='DelImg' src={image} alt="" />
-        <div className="DelTxt">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum tempora suscipit adipisci ad ab, molestiae, voluptate error atque officiis similique tenetur quisquam non et reiciendis.</div>
+        <div className="DelTxt">Allow yourself the luxury of premium delivery! Our delivery team will provide you with the highest level of service, whether you are in the office or at home. Personal couriers, exclusive packaging and flexible delivery schedules are available to you. Enjoy convenience and luxury with our premium delivery - because you deserve only the best!</div>
       </div>
     </section>
   );
