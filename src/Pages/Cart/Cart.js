5da524eeca7738320ab4a132fee3522c5077d2f0
Ilimchik <ilimbork@gmail.com>
2024-05-29 12:46:46 +0600

    adaptive cart page

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -3,23 +3,24 @@ import { useContext, useState } from "react";
 import CartList from "../../Components/CartList/CartList";
 import OrderForm from "../../Components/OrderForm/OrderForm";
 import { AppContext } from "../../App";
+import Title from "../../Components/Title/Title"
 
 export function Cart() {
   const [modalActive, setModalActive] = useState(false);
   const { cart, user } = useContext(AppContext);
-  
+
   if (Object.keys(cart).length === 0) {
     return (
-    <div className="Cart" >
-      <h1>Cart</h1>
-      <CartList />
-      <OrderForm active={modalActive} setActive={setModalActive} />
-    </div>
+      <div className="Cart" >
+        <Title>Cart</Title>
+        <CartList />
+        <OrderForm active={modalActive} setActive={setModalActive} />
+      </div>
     )
   }
   if (!user) {
     <div className="Cart" >
-      <h1>Cart</h1>
+      <Title>Cart</Title>
       <CartList />
       <OrderForm active={modalActive} setActive={setModalActive} />
     </div>
@@ -27,7 +28,7 @@ export function Cart() {
 
   return (
     <div className="Cart" >
-      <h1>Cart</h1>
+      <Title>Cart</Title>
       <CartList />
       <button className="order" onClick={() => setModalActive(true)}>Order</button>
       <OrderForm active={modalActive} setActive={setModalActive} />
