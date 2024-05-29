ca2b9838083a40f0fa116839da2262d9484f6ab6
Ilimchik <ilimbork@gmail.com>
2024-05-29 14:10:27 +0600

    add burger

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,21 +1,14 @@
-// import { slide as Menu } from 'react-burger-menu'
+import "./Burger.css";
 
-// class Example extends React.Component {
-//     showSettings(event) {
-//         event.preventDefault();
-    
-    
-    
-//     }
-//     render() {
-    
-//         return (
-//             <Menu>
-//                 <a id="home" className="menu-item" href="/">Home</a>
-//                 <a id="about" className="menu-item" href="/about">About</a>
-//                 <a id="contact" className="menu-item" href="/contact">Contact</a>
-//                 <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
-//             </Menu>
-//         );
-//     }
-// }
+
+function NavToggle({ callback }) {
+  return (
+    <button onClick={callback} className="NavToggle">
+    <div className="line"></div>
+    <div className="line"></div>
+    <div className="line"></div>                
+  </button>
+  );
+}
+
+export default NavToggle;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,31 @@
 import "./Header.css";
+import { useState } from "react";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
 import Menu from "../Menu/Menu";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
-// import Burger from "../Burger/Burger";
+import Burger from "../Burger/Burger";
+import Drawer from "../Drawer/Drawer"
 
-// const { data, isLoading } = useGetProductsQuery({ title: searchValue });
-// const [searchValue, setSearchValue] = useState("");
 
 
 export default function HeaderSection() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
 
 
 
-
-  
   return (
     <header className="Header">
       <div className="container">
         <div className="Head flex">
-          {/* <Burger /> */}
           <Logo />
+          <Burger callback={toggleDrawer} />
+          <Drawer open={drawerOpen} toggle={toggleDrawer} />
           <Menu />
           <Search />
           <div className="buttons flex">
